3db99b08da7e64c4a862ba9f3b8b2d7b8f9a2a56
devcode-gihh <mochamadsugianasaepul@gmail.com>
2026-01-28 22:31:18 +0700

    prakrikum kampus - PRTMN9 LTHN1 - belajar function void

---
+++ PERTEMUAN 9 - 01 - BELAJAR FUNCTION VOID/belajar_function_void.cpp
@@ -0,0 +1,22 @@
+// Mochamad Sugiana Saepul - 2503010087
+
+#include <iostream>
+using namespace std;
+
+void garis() {
+  cout << "------------------------------------------" << endl;
+}
+void sayHi() {
+  cout << "Hi, Selamat datang Mochamad Sugiana Saepul" << endl;
+}
+
+int main() {
+
+  cout << "\nMEMBUAT GARIS DAN UCAPAN HI LEWAT FUNCTION VOID" << endl;
+  
+  garis();
+  sayHi();
+
+  cin.get();
+  return 0;
+}
